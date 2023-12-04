f4fa9b6d3211afe61120acd450fe46d03fce2b2b
John Costa <johncosta027@gmail.com>
2023-12-04 13:31:44 +0000

    Day 4: Enjoyable !!

--- AdventOfCode2023/src/main.zig
+++ AdventOfCode2023/src/main.zig
@@ -1,13 +1,14 @@
 const std = @import("std");
 // const day1 = @import("./day1/day1.zig");
 // const day2 = @import("./day2/day2.zig");
-const day3 = @import("./day3/day3.zig");
+// const day3 = @import("./day3/day3.zig");
+const day4 = @import("./day4/day4.zig");
 const utils = @import("utils.zig");
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
-    var input = try utils.getInput("./src/day3/input.txt", allocator);
+    var input = try utils.getInput("./src/day4/input.txt", allocator);
     defer allocator.free(input);
 
-    try day3.solve(input);
+    try day4.solve(input);
 }
