a76b193a8a2b661d4fe50dc0eb56bee4016ed778
John Costa <johncosta027@gmail.com>
2023-12-06 22:25:02 +0000

    Day 6: Nice!

--- AdventOfCode2023/src/main.zig
+++ AdventOfCode2023/src/main.zig
@@ -3,13 +3,14 @@ const std = @import("std");
 // const day2 = @import("./day2/day2.zig");
 // const day3 = @import("./day3/day3.zig");
 // const day4 = @import("./day4/day4.zig");
-const day5 = @import("./day5/day5.zig");
+// const day5 = @import("./day5/day5.zig");
+const day6 = @import("./day6/day6.zig");
 const utils = @import("utils.zig");
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
-    var input = try utils.getInput("./src/day5/input.txt", allocator);
+    var input = try utils.getInput("./src/day6/input.txt", allocator);
     defer allocator.free(input);
 
-    try day5.solve(input);
+    try day6.solve(input);
 }
